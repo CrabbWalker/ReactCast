c21ca049a72c6e7a6a5a1aea03019f1e8babaf20
Darrell Wilson <darjr1991@hotmail.com>
2015-10-23 20:33:15 -0400

    Deleted Thumbnail-list and thumbnail

--- dropdown/src/app.jsx
+++ dropdown/src/app.jsx
@@ -1,26 +1,11 @@
 var React = require('react');
-var ThumbnailList = require('./thumbnail-list');
-
-var options = {
-
-thumbnailData:
-  [{
-    title: 'See Tutorials',
-    number: 32,
-    header: 'Learn React',
-    description: 'React is a JS library of complete awesomeness',
-    imageUrl: 'https://facebook.github.io/react/img/logo_og.png'
-
-  },{
-    title: 'Show Courses',
-    number: 25,
-    header: 'Learn Gulp',
-    description: 'Gulp speeds up your workflow ASAP',
-    imageUrl: 'https://avatars0.githubusercontent.com/u/6200624?v=3&s=400'
-
-}]};
-
-//it's an array that looks like this: options = {td: [{}, {}, {}]}
+var Dropdown = require('./dropdown')
+var options {
+title: 'Choose a Dessert' //what should show up on the button to open/close the dropdown
+items: [ 'Apple Pie',
+        'Peach Cobbler',
+        'Coconut Cream Pie', ]
+}
 //React will render this class
 var element= React.createElement(ThumbnailList, options);
 
---
+++ dropdown/src/dropdown.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var Button = require('./button');
+var list  = require('./list');
+
+module.exports = React.createClass({
+
+render: function()  {
+
+  return <div className="dropdown"
+  <Button /> // Lesson left off on 10:29, lecture 18
+>}
+})
